4a4c24e519ce3d56eb3952daa64b39d6788a517d
Neil Smith <smith.neil.h@gmail.com>
2020-01-24 18:56:25 -0600

    slash messages and persist deaths

--- CraigKillCounter.lua
+++ CraigKillCounter.lua
@@ -5,57 +5,110 @@ CraigKillCounter.targetLastHealth = nil;
 CraigKillCounter.currentDeaths = {};
 CraigKillCounter.currentInstance = "";
 CraigKillCounter.currentTimestamp = nil;
+CraigKillCounter.voice = true;
 
+-- SaveVariablesPerCharacter
 CraigKillCounterMCDeaths = {};
 CraigKillCounterONYDeaths = {};
 
--- MoltenCore: {
---     timestamp1: 2
---     timestamp2: 4,
--- },
--- Onyxia: {
---     timestamp1: 1,
---     timestamp2: 4
--- }
-
+SLASH_TEST1, SLASH_TEST2 = '/CraigKillCounter', '/ckc';
 
 -- TODOS
 -- 2. randomized messages
--- 3. slash commands
 
--- TESTS
--- 1. If GetNumRaidMembers works
---    /run for n=1, GetNumGroupMembers() do print(UnitName("Raid"..n));end
--- 2. If SavedVariables works in raid & party
+SlashCmdList["TEST"] = function (msg, editbox)
+    if (msg == 'deaths') then
+        CraigKillCounter:WriteKillCount();
+    elseif (msg == 'toggle') then
+        CraigKillCounter:ToggleVoice();
+        SendChatMessage("CKC Voice: "..CraigKillCounter.voice, "RAID", "COMMON");
+    end
+end
+
+function CraigKillCounter:GetPastInstanceDeaths()
+    local totalInstanceDeaths = 0;
+    if (CraigKillCounter.currentInstance == "Molten Core") then
+        for k, v in pairs(CraigKillCounterMCDeaths) do
+            totalInstanceDeaths = totalInstanceDeaths + v;
+        end
+    elseif (CraigKillCounter.currentInstance == "Onyxia's Lair") then
+        for k, v in pairs(CraigKillCounterONYDeaths) do
+            totalInstanceDeaths = totalInstanceDeaths + v;
+        end
+    end
+    return totalInstanceDeaths;
+end
+function CraigKillCounter:GetPastNonInstanceDeaths()
+    local totalInstanceDeaths = 0;
+    if (CraigKillCounter.currentInstance == "Molten Core") then
+        for k, v in pairs(CraigKillCounterONYDeaths) do
+            totalInstanceDeaths = totalInstanceDeaths + v;
+        end
+    elseif (CraigKillCounter.currentInstance == "Onyxia's Lair") then
+        for k, v in pairs(CraigKillCounterMCDeaths) do
+            totalInstanceDeaths = totalInstanceDeaths + v;
+        end
+    end
+    return totalInstanceDeaths;
+end
+
+function CraigKillCounter:ToggleVoice()
+    if (CraigKillCounter.voice == false) then
+        CraigKillCounter.voice = true;
+    else
+        CraigKillCounter.voice = false;
+    end;
+end
+
+function CraigKillCounter:WriteKillCount()
+    local deathsTonight = CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp];
+    local pastInstanceDeaths = CraigKillCounterMCDeaths:GetPastInstanceDeaths();
+    local pastNonInstanceDeaths = CraigKillCounter:GetPastNonInstanceDeaths();
+
+    local allInstanceDeaths = deathsTonight + pastInstanceDeaths;
+    local allDeaths = deathsTonight + pastInstanceDeaths + pastNonInstanceDeaths;
+
+    SendChatMessage("Near, far, wherever you are" ,"RAID" ,"COMMON");
+    SendChatMessage("I believe that the heart does, go on" ,"YELL" ,"COMMON");
+    SendChatMessage("Once more you open the door" ,"RAID" ,"COMMON");
+    SendChatMessage("And you're here in my heart" ,"RAID" ,"COMMON");
+    SendChatMessage("And my heart will go on, and on" ,"RAID" ,"COMMON");
+    SendChatMessage("----------------------------------", "RAID", "COMMON");
+    SendChatMessage("Craig has died "..deathsTonight.." times tonight", "RAID", "COMMON");
+    SendChatMessage("and has died "..allInstanceDeaths.. " in "..CraigKillCounter.currentInstance, "RAID", "COMMON");
+    SendChatMessage("for a total of "..pastNonInstanceDeaths.. " deaths in all raids.", "RAID", "COMMON");
+    SendChatMessage("----------------------------------", "RAID", "COMMON");
+end
 
 function CraigKillCounter:HandleTargetHealthChange(health)
     local maxHealth = UnitHealthMax(CraigKillCounter.target);
     local healthPercent = (health / maxHealth) * 100;
 
     if (health <= 0) then
-        SendChatMessage("Near, far, wherever you are" ,"YELL" ,"COMMON");
-        SendChatMessage("I believe that the heart does, go on" ,"YELL" ,"COMMON");
-        SendChatMessage("Once more you open the door" ,"YELL" ,"COMMON");
-        SendChatMessage("And you're here in my heart" ,"YELL" ,"COMMON");
-        SendChatMessage("And my heart will go on, and on" ,"YELL" ,"COMMON");
+        -- update currentDeats
+        if (CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp] == nil) then
+            CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp] = 0;
+        end
+        CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp] = 1 + CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp];
+
+        -- update SavedVariable for current raid instance
         if (CraigKillCounter.currentInstance == "Molten Core") then
-            if (CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp] == nil) then
-                CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp] = 0;
-            end
             if (CraigKillCounterMCDeaths[timestamp] == nil) then
                 CraigKillCounterMCDeaths[timestamp] = 0;
             end
-            CraigKillCounter.currentDeaths[CraigKillCounter.currentTimestamp] += 1;
+            CraigKillCounterMCDeaths[timestamp] = 1 + CraigKillCounterMCDeaths[timestamp];
         end
-    elseif (healthPercent <= 20) then
-        -- SendChatMessage(CraigKillCounter.target.." BOUT TO DIE" ,"YELL" ,"COMMON");
+
+        if (CraigKillCounter.voice) then
+            CraigKillCounter:WriteKillCount();
+        end;
     end
 end
 
 function CraigKillCounter:HandleGroupRosterUpdate()
     for n=1, GetNumGroupMembers() do
         local pname = UnitName("Raid"..n);
-        if pname == CraigKillCounter.target then
+        if (pname == CraigKillCounter.target) then
             CraigKillCounter.targetGrouped = true;
             return;
         end
@@ -66,7 +119,7 @@ function CraigKillCounter:HandleGroupRosterUpdate()
 end
 
 function CraigKillCounter:HandleCombatEvent()
-    if (CraigKillCounter.targetFound == false) then
+    if (CraigKillCounter.targetGrouped == false) then
         return;
     end
     if (CraigKillCounter.currentInstance == "") then
